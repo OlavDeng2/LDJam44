4ac589c0974eb0ab58e86ef4c96ac6270862b4ef
OlavDeng2 <olavkruithof@gmail.com>
2019-05-15 21:33:39 +0200

    Added little test to see if Hybrid ECS works (it works)

---
+++ Assets/Scripts/Systems/BulletSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class BulletSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((Bullet bullet) => {
+            Debug.Log("It is working");
+        });
+    }
+}
