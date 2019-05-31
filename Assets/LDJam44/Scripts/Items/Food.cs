ca34cfe14514c488367356b8d0cdc95fc36543cd
OlavDeng2 <olavkruithof@gmail.com>
2019-05-31 20:23:55 +0200

    created basic item class

--- Assets/LDJam44/Scripts/Items/Food.cs
+++ Assets/LDJam44/Scripts/Items/Food.cs
@@ -2,43 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Food : MonoBehaviour, IInventoryItem
-{
-    public string Name
-    {
-        get
-        {
-            return "Food";
-        }
-    }
-
-    public Sprite image = null;
-
-    public Sprite Image
-    {
-        get
-        {
-            return image;
-        }
-    }
-
-    public void OnPickup()
-    {
-        //Do something when picked up
-    }
-
-    public void OnDrop()
-    {
-        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        gameObject.SetActive(true);
-        gameObject.transform.position = worldPoint;
-    }
-
-    public void OnUse()
-    {
-        //Do something when used
-    }
-
-   
+public class Food : Item
+{  
 }
---
+++ Assets/LDJam44/Scripts/Items/Item.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour, IInventoryItem
+{
+    public string name;
+    public Sprite image;
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+    }
+
+    public Sprite Image
+    {
+        get
+        {
+            return image;
+        }
+    }
+
+    public void OnDrop()
+    {
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        gameObject.SetActive(true);
+        gameObject.transform.position = worldPoint;
+    }
+
+    public void OnPickup()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public virtual void OnUse()
+    {
+        throw new System.NotImplementedException();
+    }
+}
