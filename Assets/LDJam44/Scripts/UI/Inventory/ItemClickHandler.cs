c35411442ac690f8aac35da89e820c3b9e80512d
OlavDeng2 <olavkruithof@gmail.com>
2019-06-02 15:58:55 +0200

    added first basic selection of item using keypresses

--- Assets/LDJam44/Scripts/UI/Inventory/ItemClickHandler.cs
+++ Assets/LDJam44/Scripts/UI/Inventory/ItemClickHandler.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemClickHandler : MonoBehaviour
 {
     public Inventory inventory;
+
+    public KeyCode key;
+    private Button button;
+
     public void Start()
     {
         inventory = FindObjectOfType<Inventory>();
+        button = GetComponent<Button>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(key))
+        {
+            button.onClick.Invoke();
+        }
     }
     // Start is called before the first frame update
     public void OnItemClicked()
