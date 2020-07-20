4246e2f691ce49a86769fbadd0af10851f1ecd8a
OlavDeng2 <olavkruithof@gmail.com>
2020-07-20 17:06:41 +0200

    Implemented a more intelligent zombie spawning system

--- Assets/LDJam44/Scripts/Management/GameManager.cs
+++ Assets/LDJam44/Scripts/Management/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -12,6 +13,7 @@ public class GameManager : MonoBehaviour
     public int initialZombies = 1; // This is per spawnpoint per time it is allowed to spawn in a round, multiplied by round count
     public float timeToNextRound = 10f; //time between rounds, will be multiplied by round count
     public float roundTime = 60f; // The amount of time in a round, in seconds. Will be multiplied by round count
+    public int enemyWaves = 6;
     public float currentTime = 0f;
     public float spawnTime = 5f;
     public float timeSinceLastSpawn = 0f;
@@ -26,7 +28,9 @@ public class GameManager : MonoBehaviour
     [Header("Zombie Settings")]
     public ObjectPool zombiePool;
     public SpawnPoint[] zombieSpawnPoints;
-    //public int zombiesToSpawn = 5;
+    public int[] zombiesPerRound;
+    private float timePerWave = 0f;
+    private int zombiesPerWave = 0;
 
     [Header("Game Data")]
     public PooledObject player;
@@ -39,6 +43,8 @@ public class GameManager : MonoBehaviour
     {
         SpawnPlayer();
         Time.timeScale = 1;
+
+        timePerWave = roundTime / enemyWaves;
 
         //SpawnEnemies(zombieSpawnPoints, initialZombies * currentRound, zombiePool);
 
@@ -80,20 +86,31 @@ public class GameManager : MonoBehaviour
             }
 
             shopScript.RefreshStore();
+
+            
+            //Only change the zombies to spawn per wave if there is a new number to change to
+            if (zombiesPerRound.Length > currentRound)
+            {
+                zombiesPerWave = zombiesPerRound[currentRound] / enemyWaves;
+            }
+            else
+            {
+                return;
+            }
+
+            StartCoroutine(ZombieSpawn(timePerWave, zombiesPerWave));
+
         }
 
         //spwn enemies if you can spawn enemies and the round time isnt over yet
         if (spawnEnemies && currentTime <= roundTime * currentRound)
         {
-            if(timeSinceLastSpawn > spawnTime)
-            {
-                SpawnEnemies(zombieSpawnPoints, initialZombies * currentRound, zombiePool);
-                timeSinceLastSpawn = 0f;
-            }
-
+            
             timeSinceLastSpawn += Time.deltaTime;
             currentTime += Time.deltaTime;
         }
+
+        //End the round
 
         else if (spawnEnemies && currentTime >= roundTime * currentRound)
         {
@@ -122,17 +139,24 @@ public class GameManager : MonoBehaviour
         player.transform.position = randomSpawnPos.position;
     }
 
-    private void SpawnEnemies(SpawnPoint[] allSpawnpoints, int amountToSpawn, ObjectPool objectPool)
+    IEnumerator ZombieSpawn(float time, int zombiesToSpawn)
     {
-        foreach(SpawnPoint spawnPoint in allSpawnpoints)
+        while (spawnEnemies)
         {
-            for(int i = 0; i < spawnPoint.amountToSpawn; i++)
+            for (int i = 0; i <= zombiesToSpawn; i++)
             {
-                PooledObject enemy = objectPool.GetObject();
+
+                Debug.Log("Spawned zombie");
+                SpawnPoint spawnPoint = zombieSpawnPoints[Random.Range(0, zombieSpawnPoints.Length - 1)];
+                PooledObject enemy = zombiePool.GetObject();
                 enemy.GetComponent<Enemy>().gameManager = this;
                 enemy.GetComponent<Enemy>().alive = true;
+                enemy.GetComponent<Enemy>().health = enemy.GetComponent<Enemy>().defaultHealth;
                 enemy.transform.position = spawnPoint.transform.position;
+
             }
+
+            yield return new WaitForSeconds(12);
         }
     }
 
